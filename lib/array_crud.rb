c8bfa3853524d0cee513ea0b14e7cc8cb1fbd0f9
John Reyes <john.jreye@gmail.com>
2019-09-20 17:24:33 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -18,9 +18,8 @@ def remove_element_from_end_of_array(array)
   array.pop
 end
 
-def pieces_of_array_remove_front
-  pieces_of_array_remove_front = ["variables", "array", "element"]
-  variables = pieces_of_array_remove_front.shift
+def remove_element_from_start_of_array(array)
+  array.shift
 end
 
 def retrieve_element_from_index
